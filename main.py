19167e79e332d96670c250c430fa245441ab6836
MURATTAS <taasmuraat@gmail.com>
2020-11-16 21:20:00 +0300

    file

---
+++ main.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+from tkinter.ttk import *
+
+from time import strftime
+
+root = Tk()
+
+root.title("Clock")
+
+def time():
+    string = strftime("%H:%M:%S")
+    label.config(text=string)
+    label.after(1000,time)
+
+label = Label(root, font=("Times New Roman",80), background = "green", foreground = "white")
+label.pack(anchor="center")
+time()
+
+mainloop()
